87ef03f31af0d8e0d09f897062a83f34d614d4ae
Christian Pila <christianpila25@gmail.com>
2026-07-05 13:04:51 +0800

    feat: add priority and due date columns to todos with corresponding SQL type and snapshot

---
+++ drizzle/0002_narrow_lord_tyger.sql
@@ -0,0 +1,3 @@
+CREATE TYPE "public"."priority" AS ENUM('low', 'medium', 'high');--> statement-breakpoint
+ALTER TABLE "todos" ADD COLUMN "priority" "priority" DEFAULT 'medium';--> statement-breakpoint
+ALTER TABLE "todos" ADD COLUMN "due_date" date;
